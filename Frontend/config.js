9238e44baea323ca23ee861a31d828e78aa7b321
tuan-coops <tuanva270605@gmail.com>
2025-11-07 22:21:49 +0700

    fix: update frontend pages

--- Frontend/config.js
+++ Frontend/config.js
@@ -3,6 +3,6 @@ const API_BASE_URL =
   window.location.hostname === "127.0.0.1" ||
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000"        // local backend
-    : "https://forum-backend.onrender.com"; // Render backend
+    : "https://forum-web-5dsw.vercel.app/"; // Render backend
 
 console.log("🔗 API_BASE_URL =", API_BASE_URL);
--- Frontend/firebase-config.js
+++ Frontend/firebase-config.js
@@ -30,7 +30,7 @@ export const API_BASE_URL =
   window.location.hostname === "127.0.0.1" ||
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000"        // local backend
-    : "https://forum-backend.onrender.com"; // Render backend
+    : "https://forum-web-5dsw.vercel.app/"; // Render backend
 
 
 
